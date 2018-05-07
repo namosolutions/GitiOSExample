8363e3986dd0b80a2994f371f5a74fee3f2895c3
Milpitas <milpitas@milpitass-macbook-air.local>
2018-05-07 14:54:13 +0530

    Add new map-for-itinerary feature

---
+++ GitiOSExample/MapConceptViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapConceptViewController.swift
+//  GitiOSExample
+//
+//  Created by Milpitas on 5/7/18.
+//  Copyright © 2018 Milpitas. All rights reserved.
+//
+
+import UIKit
+
+class MapConceptViewController: NSObject {
+
+}
